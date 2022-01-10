c5b5336797e156b99ab4364c6bda6809fbfa847b
AlirezaHabibiCE <adabadabdasht@gmail.com>
2022-01-10 21:52:33 +0330

    fixing cors for local host

--- patogh_conf/patogh/settings.py
+++ patogh_conf/patogh/settings.py
@@ -70,9 +70,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'https://api.patogh.markop.ir',
-    "http://localhost:4000",
+    "http://localhost:3000/enteremail",
+    "http://localhost:3000/signup",
     "http://127.0.0.1:8000",
-    "http://127.0.0.1:4000",
     'https://patogh.markop.ir/enteremail',
     'https://patogh.markop.ir/signup',
 ]
