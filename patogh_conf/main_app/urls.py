3aa9a6935de44c9f4d3cf9d4d47e29bee2163844
Farzady666 <farzad.y666@gmail.com>
2022-01-14 03:05:24 +0330

    UserPartiesPath added

--- patogh_conf/main_app/urls.py
+++ patogh_conf/main_app/urls.py
@@ -18,5 +18,8 @@ urlpatterns = [
 
     #user paths
     path('profile/',UserInfoApiView.as_view(), name = 'user_info' ),
-    path('userprofile/', UserProfileView.as_view())
+    path('userprofile/', UserProfileView.as_view()),
+
+    # users parties
+    path('parties/', UserParties.as_view(), name="user_parties"),
 ]
