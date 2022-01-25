d596be268a0453a8460d6b89672531e8b605facf
AlirezaHabibiCE <adabadabdasht@gmail.com>
2022-01-25 17:56:10 +0330

    add url

--- patogh_conf/patogh/settings.py
+++ patogh_conf/patogh/settings.py
@@ -76,6 +76,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     'http://localhost:3000',
+    'http://0.0.0.0:8000',
     # 'https://patogh.markop.ir/enteremail',
     # 'https://patogh.markop.ir/signup',
     # 'http://localhost:3000/signin',
