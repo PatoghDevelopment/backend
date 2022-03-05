d8a4129a894f7d416e57bb0aa84159d92b8b073b
Saeed Vajdi <saeedvajdi79@gmail.com>
2022-03-05 14:27:53 +0330

    delete

--- patogh_conf/main_app/filters.py
+++
@@ -1,9 +0,0 @@
-from django_filters import rest_framework as filters
-from .models import PatoghInfo
-
-
-class PatoghInfoFilter(filters.FilterSet):
-
-    class Meta:
-        model = PatoghInfo
-        fields = ['name','city']
