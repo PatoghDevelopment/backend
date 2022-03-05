11fa120597c2376f46b5db8f27576d6c01dde9af
Saeed Vajdi <saeedvajdi79@gmail.com>
2022-03-05 14:29:30 +0330

    add production

--- patogh_conf/main_app/serializers.py
+++ patogh_conf/main_app/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth import authenticate
 from rest_framework import serializers
 from .models import *
 from django.utils.translation import gettext_lazy as _
+from rest_framework.generics import get_object_or_404
+from django.db.models import Q
 from django.utils import timezone
 import datetime
 
@@ -10,17 +12,62 @@ class EmailSerializer(serializers.Serializer):
     email = serializers.EmailField()
 
 
-from .models import Patogh
+class EmailSerializerResetPassword(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        
+        if email:
+            if not User.objects.filter(email=email).exists():
+                msg = _("کاربر با این مشخصات وجود ندارد")
+                raise serializers.ValidationError(msg, code= 'authorization')
+        #     else:
+        #         msg = _("کاربر با این مشخصات وجود ندارد")
+        #         raise serializers.ValidationError(msg, code= 'authorization')
+        # else:
+        #     msg = _("اطلاعات کابر باید به درستی و کامل وارد شود")
+        #     raise serializers.ValidationError(msg, code = 'authorization')
+
+        # if user1:
+        #     attrs['user'] = user1
+        return attrs
+    
+class EmailSerializerSignup(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        
+        if email:
+            if User.objects.filter(email=email).exists():
+                msg = _("کاربر با این مشخصات وجود دارد")
+                raise serializers.ValidationError(msg, code= 'authorization')
+        #     else:
+        #         msg = _("کاربر با این مشخصات وجود ندارد")
+        #         raise serializers.ValidationError(msg, code= 'authorization')
+        # else:
+        #     msg = _("اطلاعات کابر باید به درستی و کامل وارد شود")
+        #     raise serializers.ValidationError(msg, code = 'authorization')
+
+        # if user1:
+        #     attrs['user'] = user1
+
+        return attrs
+
+
+
 
 
 class SignupSerializer(serializers.Serializer):
+
     email = serializers.EmailField(write_only=True)
 
     otp = serializers.CharField(
         label=_("توکن"),
         write_only=True
     )
-
+    
     password1 = serializers.CharField(
         label=_("1رمز عبور"),
         min_length=6,
@@ -28,7 +75,7 @@ class SignupSerializer(serializers.Serializer):
         write_only=True,
         help_text=_("رمز عبور باید حداقل 6 کاراکتر باشد")
     )
-
+    
     password2 = serializers.CharField(
         label=_("2رمز عبور"),
         min_length=6,
@@ -76,56 +123,60 @@ class SignupSerializer(serializers.Serializer):
 
 
 class SigninSerializer(serializers.Serializer):
-    username = serializers.CharField(
-        label=_("نام کاربری"),
-        write_only=True
+    email = serializers.EmailField(
+        label=_("ایمیل"),
+        write_only=True        
     )
 
     password = serializers.CharField(
-        label=_("رمز عبور"),
-        min_length=6,
-        write_only=True,
-        help_text=_("رمز عبور باید حداقل 6 کاراکتر باشد")
+        label= _("رمز عبور"),
+        min_length = 6,
+        write_only = True,
+        help_text =_("رمز عبور باید حداقل 6 کاراکتر باشد")
     )
 
     token = serializers.CharField(
-        label=_("توکن"),
-        read_only=True
+        label = _("توکن"),
+        read_only = True
     )
 
     def validate(self, attrs):
-        username = attrs.get('username')
+        email = attrs.get('email')
         password = attrs.get('password')
+        
+        # user = User.objects.get(email=email)
+        # if user:
+        #     if user.check_password(password):
+        #         attrs['user'] = user
+        #     else:
+        #         msg = _("رمز عبور اشتباه است")
+        #         raise serializers.ValidationError(msg, code= 'authorization')
+        # else:
+        #     msg = _("کاربر با این مشخصات وجود ندارد")
+        #     raise serializers.ValidationError(msg, code= 'authorization')
 
-        if username and password:
-            user1 = authenticate(request=self.context.get('request'),
-                                 username=username, password=password)
+        
+        if email and password :
 
-            if User.objects.filter(email=username).exists():
-                user2 = User.objects.get(email=username)
+            if User.objects.filter(email=email, password = password).exists():
+                user1 = User.objects.get(email = email)
 
-            if not (user1 or user2):
+            elif User.objects.filter(email=email).exists() :
+                user = User.objects.filter(email=email).first()
+                msg = _("رمز عبور اشتباه است")
+                raise serializers.ValidationError(msg, code= 'authorization')
+            else:
                 msg = _("کاربر با این مشخصات وجود ندارد")
-                raise serializers.ValidationError(msg, code='authorization')
+                raise serializers.ValidationError(msg, code= 'authorization')
         else:
             msg = _("اطلاعات کابر باید به درستی و کامل وارد شود")
-            raise serializers.ValidationError(msg, code='authorization')
+            raise serializers.ValidationError(msg, code = 'authorization')
 
         if user1:
             attrs['user'] = user1
-        elif user2:
-            attrs['user'] = user2
 
         return attrs
 
-
-class UserSerializer(serializers.ModelSerializer):
-    city = serializers.PrimaryKeyRelatedField(queryset=City.objects.all(), many=True, required=False)
-
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'gender_status', 'email', 'birth_date', 'city', 'avatar', 'mobile_number',
-                  'bio']
 
 
 class VerifyOTPSerializer(serializers.ModelSerializer):
@@ -145,7 +196,7 @@ class RestPasswordSerializer(serializers.Serializer):
         password1 = attrs.get('password1')
         password2 = attrs.get('password2')
         user = User.objects.filter(email=email).first()
-        if user:
+        if User.objects.filter(email=email).exists():
             if password1 != password2:
                 msg = _("لطفا هردو گذرواژه را یکسان وارد نمایید")
                 raise serializers.ValidationError(msg, code='conflict')
@@ -167,21 +218,33 @@ class RestPasswordSerializer(serializers.Serializer):
 class CityListSerializer(serializers.ModelSerializer):
     class Meta:
         model = City
-        fields = ['id', 'name']
+        fields = ['id','name']
         read_only_fields = ['id']
 
+class UserSerializer(serializers.ModelSerializer):
 
-class UserProfileSerializer(serializers.ModelSerializer):
+    city = serializers.PrimaryKeyRelatedField(queryset=City.objects.all(), many=True, required=False)
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'gender_status', 'email', 'birth_date', 'city', 'avatar', 'mobile_number',
-                  'bio', 'score']
+        fields = ['username','first_name', 'last_name', 'gender', 'email', 'birth_date', 'city', 'avatar', 'mobile_number', 'bio', 'score']
         read_only_fields = ['email']
+
+    def validate(self, attrs):
+        if attrs['username']:
+            username = attrs.get('username')
+            if User.objects.filter(username=username).exists():
+                msg = _("کاربر با این نام کاربری وجود دارد")
+                raise serializers.ValidationError(msg, code= 'authorization')
+
+        return attrs
+   
 
 
 # Patogh start----------------------------------------------------------------------------------
 
 class PatoghInfoSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = PatoghInfo
         fields = "__all__"
@@ -189,13 +252,13 @@ class PatoghInfoSerializer(serializers.ModelSerializer):
 
 class PatoghSerializer(serializers.ModelSerializer):
     patoghinfo = PatoghInfoSerializer()
-
     class Meta:
         model = Patogh
         fields = "__all__"
 
 
 class PatoghInfoLimitedSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = PatoghInfo
         fields = ('name', 'city')
@@ -203,17 +266,20 @@ class PatoghInfoLimitedSerializer(serializers.ModelSerializer):
 
 class PatoghLimitSerializer(serializers.ModelSerializer):
     patoghinfo = PatoghInfoLimitedSerializer()
-
     class Meta:
         model = Patogh
-        fields = ('id', 'start_time', 'patoghinfo')
+        fields = ('id','start_time','patoghinfo')
 
+
+class PatoghSerializerCalledByInfo(serializers.ModelSerializer):
+    class Meta:
+        model = Patogh
+        fields = "__all__"
 
 class PatoghHaveImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = PatoghHaveImages
         fields = "__all__"
-
 
 class PatoghMembersSerializer(serializers.ModelSerializer):
     class Meta:
@@ -222,10 +288,9 @@ class PatoghMembersSerializer(serializers.ModelSerializer):
 
 
 class PatoghAndOtherModelSerializer(serializers.ModelSerializer):
-    patogh = PatoghSerializer()
+    patogh = PatoghSerializerCalledByInfo()
     patoghhaveimages = PatoghHaveImagesSerializer()
     patoghmembers = PatoghMembersSerializer()
-
     class Meta:
         model = PatoghInfo
         fields = '__all__'
@@ -234,18 +299,20 @@ class PatoghAndOtherModelSerializer(serializers.ModelSerializer):
         patogh = validated_data.pop('patogh')
         patoghhaveimages = validated_data.pop('patoghhaveimages')
         patoghmembers = validated_data.pop('patoghmembers')
-        patoghinfo = PatoghInfo.objects.create(**validated_data)
-        Patogh.objects.create(patogh_id_id=patoghinfo, **patogh)
-        PatoghHaveImages.objects.create(patogh_id_id=patoghinfo, **patoghhaveimages)
-        PatoghMembers.objects.create(patogh_id_id=patoghinfo, **patoghmembers)
+        patoghinfo = super().create(validated_data)
+        print(patoghinfo+"asfdsajdflasbfoahsfohasfas;iugsafiuvasfi")
+        Patogh.objects.create(patogh_id=patoghinfo['id'], **patogh)
+        PatoghHaveImages.objects.create(patogh_id=patoghinfo, **patoghhaveimages)
+        PatoghMembers.objects.create(patogh_id=patoghinfo, **patoghmembers)
+        
 
         return patoghinfo
-
+    
     def update(self, instance, validated_data):
         patoghs_data = validated_data.pop('patogh')
         patoghhaveimages_data = validated_data.pop('patoghhaveimages')
         patoghmembers_data = validated_data.pop('patoghmembers')
-
+        
         patoghs = (instance.patogh).all()
         patoghs = list(patoghs)
 
@@ -253,8 +320,9 @@ class PatoghAndOtherModelSerializer(serializers.ModelSerializer):
         patoghhaveimages = list(patoghhaveimages)
 
         patoghmemberss = (instance.patoghmembers).all()
-        patoghmemberss = list(patoghmemberss)
+        patoghmemberss  = list(patoghmemberss)  
 
+        
         instance.name = validated_data.get('name', instance.name)
         instance.profile_image = validated_data.get('profile_image', instance.profile_image)
         instance.description = validated_data.get('description', instance.description)
@@ -271,23 +339,22 @@ class PatoghAndOtherModelSerializer(serializers.ModelSerializer):
             patogh.start_time = patogh_data.get('start_time', patogh.start_time)
             patogh.end_time = patogh_data.get('end_time', patogh.end_time)
             patogh.save()
-
+        
         for patoghhaveimage_data in patoghhaveimages_data:
             patoghhaveimage = patoghhaveimages.pop(0)
             patoghhaveimage.image_url = patoghhaveimage_data.get('image_url', patoghhaveimage.image_url)
             patoghhaveimage.status = patoghhaveimage_data.get('status', patoghhaveimage.status)
             patoghhaveimage.send_time = patoghhaveimage_data.get('send_time', patoghhaveimage.send_time)
             patoghhaveimage.save()
-
+        
         for patoghmember_data in patoghmembers_data:
             patoghmembers = patoghmemberss.pop(0)
-            patoghmembers.state = patoghmember_data.get('state', patoghmembers.state)
-            patoghmembers.time = patoghmember_data.get('time', patoghmembers.time)
-            patoghmembers.email_id = patoghmember_data.get('email_id', patoghmembers.email_id)
+            patoghmembers.state =  patoghmember_data.get('state', patoghmembers.state)
+            patoghmembers.time =  patoghmember_data.get('time', patoghmembers.time)
+            patoghmembers.email_id =  patoghmember_data.get('email_id', patoghmembers.email_id)
             patoghmembers.save()
-
+        
         return instance
-
 
 # Patogh end ----------------------------------------------------------------------------------
 
@@ -301,3 +368,41 @@ class UserPartiesSerializer(serializers.ModelSerializer):
     class Meta:
         model = PartyMembers
         fields = ('id', 'name', 'description', 'avatar')
+
+
+
+
+
+
+
+# class PatoghInfoListCreateSerializer(serializers.ModelSerializer):
+#     patogh = PatoghSerializerCalledByInfo()
+#     patoghhaveimages = PatoghHaveImagesSerializer()
+#     patoghmembers = PatoghMembersSerializer()
+
+    # class Meta:
+    #     model = PatoghInfo
+    #     fields = '__all__'
+    #     read_only_fields = ['id', 'creator']
+
+    # def create(self, validated_data):
+    #     user = self.context['request'].user
+    #     id = validated_data['id']
+    #     patogh = validated_data.pop('patogh')
+    #     patoghhaveimages = validated_data.pop('patoghhaveimages')
+    #     patoghmembers = validated_data.pop('patoghmembers')
+    #     validated_data['creator'] = user
+    #     patoghinfo = super().create(validated_data)
+    #     Patogh.objects.create(patogh_id=id, **patogh)
+    #     PatoghHaveImages.objects.create(patogh_id=id, **patoghhaveimages)
+    #     PatoghMembers.objects.create(patogh_id=id, **patoghmembers)
+
+        # return patoghinfo
+
+    # def update(self, instance, validated_data):
+    #     status = validated_data.get('status', None)
+    #     if status:
+    #         instance.status = status
+    #         instance.save()
+    #     return instance
+
--- patogh_conf/patogh/urls.py
+++ patogh_conf/patogh/urls.py
@@ -15,23 +15,23 @@ Including another URLconf
 """
 from os import stat
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.utils.translation import gettext as _
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
-                  path('main_app/', include('main_app.urls')),
+    path('main_app/', include('main_app.urls')),
 
-                  path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-                  path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-                  path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 APP_NAME = _("پاتوق")
-admin.site.site_header = "پنل مدیریت " + APP_NAME
+admin.site.site_header = _("پنل مدیریت") + APP_NAME
 admin.site.site_title = APP_NAME
-admin.site.index_title = 'پنل مدیریت '
+admin.site.index_title = _("پنل مدیریت")
