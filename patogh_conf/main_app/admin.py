133be3471d94d2e0e702abb532203c8181bc57f3
ASUS <saeedvajdi79@gmail.com>
2022-03-06 00:23:28 +0330

    edit models

--- patogh_conf/main_app/admin.py
+++ patogh_conf/main_app/admin.py
@@ -8,128 +8,130 @@ from .models import *
 # Register your models here 
 class CityAdmin(admin.ModelAdmin):
     ordering = ['name']
-    list_display = ('id','name')
-    search_fields = ['name']
-    
-class PatoghCategoryAdmin(admin.ModelAdmin):
-    ordering = ['name']
-    list_display = ('id','name')
+    list_display = ('id', 'name')
     search_fields = ['name']
 
-class LocationTypesAdmin(admin.ModelAdmin):
+
+class PatoghCategoryAdmin(admin.ModelAdmin):
     ordering = ['name']
-    list_display = ('id','name')
+    list_display = ('id', 'name')
     search_fields = ['name']
-    list_filter = []
 
 
 class TagsAdmin(admin.ModelAdmin):
     ordering = ['tag']
-    list_display = ('tag','id')
+    list_display = ('tag', 'id')
     search_fields = ['tag']
     list_filter = []
 
-    
+
 class UserAdmin(admin.ModelAdmin):
     ordering = ['username']
-    list_display = ('username','last_name','email','mobile_number','gender')
-    search_fields = ['username','first_name','last_name','email']
+    list_display = ('username', 'last_name', 'email', 'mobile_number', 'gender')
+    search_fields = ['username', 'first_name', 'last_name', 'email']
     list_filter = ['gender']
     autocomplete_fields = ['city']
     fieldsets = (
-        (None, {'fields': ('username','email','mobile_number','password')}),
-        (_("اصل"), {'fields': ('birth_date','first_name','last_name','city')}),
-        (_("اطلاعت دیگر"),{'fields': ('gender','avatar','bio','score')}),
+        (None, {'fields': ('username', 'email', 'mobile_number', 'password')}),
+        (_("اصل"), {'fields': ('birth_date', 'first_name', 'last_name', 'city')}),
+        (_("اطلاعت دیگر"), {'fields': ('gender', 'avatar', 'bio', 'score')}),
     )
+
 
 class PatoghInfoAdmin(admin.ModelAdmin):
     ordering = ['name']
-    list_display = ('name','type','city','creator')
-    search_fields = ['creator','name','city','category']
+    list_display = ('name', 'type', 'city', 'creator')
+    search_fields = ['creator', 'name', 'city', 'category']
     list_filter = ['type']
-    autocomplete_fields = ['city','creator','category']
+    autocomplete_fields = ['city', 'creator', 'category']
     fieldsets = (
-        (None, {'fields': ('id','creator','name','type')}),
-        (_("آدرس"),{'fields': ('address','city')}),
-        (_("اطلاعات دیگر"),{'fields': ('category','description','creation_time','profile_image')})
+        (None, {'fields': ('id', 'creator', 'name', 'type')}),
+        (_("آدرس"), {'fields': ('address', 'city')}),
+        (_("اطلاعات دیگر"), {'fields': ('category', 'description', 'creation_time', 'profile_image')})
     )
-    readonly_fields =['creation_time']
+    readonly_fields = ['creation_time']
+
 
 class PendingVerifyAdmin(admin.ModelAdmin):
     ordering = ['send_time']
-    list_display = ('send_time','receptor','allowed_try')
+    list_display = ('send_time', 'receptor', 'allowed_try')
     list_filter = ['allowed_try']
     search_fields = ['receptor']
     list_filter = []
 
+
 class PartyAdmin(admin.ModelAdmin):
-    ordering = ['name','id']
-    list_display = ('name','id','creator','creation_time')
+    ordering = ['name', 'id']
+    list_display = ('name', 'id', 'creator', 'creation_time')
     search_fields = ['creator']
     list_filter = []
     autocomplete_fields = ['creator']
     fieldsets = (
-        (None, {'fields': ('id','creator_id','patogh_id','name','status')}),
-        (_("فیلتر ها"), {'fields': ('start_time','end_time','gender_filter','members_count','min_age','max_age')}),
-        (_("اطلاعات دیگر"),{'fields': ('description','tags_id')})
+        (None, {'fields': ('id', 'creator_id', 'patogh_id', 'name', 'status')}),
+        (_("فیلتر ها"), {'fields': ('start_time', 'end_time', 'gender_filter', 'members_count', 'min_age', 'max_age')}),
+        (_("اطلاعات دیگر"), {'fields': ('description', 'tags_id')})
     )
-    readonly_fields =['creation_time']
+    readonly_fields = ['creation_time']
 
 
 class PatoghAdmin(admin.ModelAdmin):
-    ordering = ['patogh_id','id']
-    list_display = ('patogh_id','id','start_time','end_time')
+    ordering = ['patogh_id', 'id']
+    list_display = ('patogh_id', 'id', 'start_time', 'end_time')
     search_fields = ['patogh_id']
     list_filter = []
     autocomplete_fields = ['patogh_id']
 
+
 class PartyMembersAdmin(admin.ModelAdmin):
-    ordering = ['p_id','g_id']
-    list_display = ('p_id','g_id','status')
+    ordering = ['p_id', 'g_id']
+    list_display = ('p_id', 'g_id', 'status')
     search_fields = ['status']
     list_filter = []
-    autocomplete_fields = ['p_id','g_id']
+    autocomplete_fields = ['p_id', 'g_id']
+
 
 class PatoghMembersAdmin(admin.ModelAdmin):
-    ordering = ['patogh_id','email']
-    list_display = ('patogh_id','email','state','time')
+    ordering = ['patogh_id', 'email']
+    list_display = ('patogh_id', 'email', 'state', 'time')
     search_fields = ['state']
     list_filter = []
-    autocomplete_fields = ['patogh_id','email']
+    autocomplete_fields = ['patogh_id', 'email']
+
 
 class PatoghsCommentsAdmin(admin.ModelAdmin):
     ordering = ['id']
-    list_display = ('sender','patogh_id','send_time')
-    search_fields = ['sender','patogh_id']
+    list_display = ('sender', 'patogh_id', 'send_time')
+    search_fields = ['sender', 'patogh_id']
     list_filter = []
-    autocomplete_fields = ['sender','patogh_id','reply_to']
+    autocomplete_fields = ['sender', 'patogh_id', 'reply_to']
+
 
 class reportedPatoghAdmin(admin.ModelAdmin):
-    ordering = ['username','patogh_id']
-    list_display = ('username','patogh_id','send_time')
-    search_fields = ['sender','patogh_id']
+    ordering = ['username', 'patogh_id']
+    list_display = ('username', 'patogh_id', 'send_time')
+    search_fields = ['sender', 'patogh_id']
     list_filter = []
-    autocomplete_fields = ['patogh_id','username']
+    autocomplete_fields = ['patogh_id', 'username']
+
 
 class PatoghHaveImagesAdmin(admin.ModelAdmin):
     ordering = ['patogh_id']
-    list_display = ('patogh_id','status','send_time')
+    list_display = ('patogh_id', 'status', 'send_time')
     search_fields = ['patogh_id']
     list_filter = ['status']
     autocomplete_fields = ['patogh_id']
 
+
 class UsersHaveFriendsAdmin(admin.ModelAdmin):
-    ordering = ['sender','receiver']
-    list_display = ('sender','receiver','state','time')
-    search_fields = ['sender','receiver','state']
+    ordering = ['sender', 'receiver']
+    list_display = ('sender', 'receiver', 'state', 'time')
+    search_fields = ['sender', 'receiver', 'state']
     list_filter = ['state']
-    autocomplete_fields = ['sender','receiver']
+    autocomplete_fields = ['sender', 'receiver']
 
 
-
-admin.site.register(City ,CityAdmin)
-admin.site.register(PatoghCategory ,PatoghCategoryAdmin)
-admin.site.register(LocationTypes, LocationTypesAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(PatoghCategory, PatoghCategoryAdmin)
 admin.site.register(Tags, TagsAdmin)
 admin.site.register(User, UserAdmin)
 admin.site.register(PatoghInfo, PatoghInfoAdmin)
@@ -142,6 +144,3 @@ admin.site.register(PatoghsComments, PatoghsCommentsAdmin)
 admin.site.register(reportedPatogh, reportedPatoghAdmin)
 admin.site.register(PatoghHaveImages, PatoghHaveImagesAdmin)
 admin.site.register(UsersHaveFriends, UsersHaveFriendsAdmin)
-
-
-
--- patogh_conf/main_app/models.py
+++ patogh_conf/main_app/models.py
@@ -1,26 +1,11 @@
-from enum import auto
 from django.contrib.auth.base_user import BaseUserManager
-from django.core import validators
-from django.db import models, transaction
+from django.db import models
 import uuid
-from datetime import date
-import hashlib
-from django.db.models import indexes
-from django.db.models.aggregates import Max
 from django.db.models.base import Model
-from django.db.models.deletion import CASCADE, PROTECT
-from django.db.models.fields import CharField
-from django.contrib.auth.models import AbstractUser, UserManager
-from django.template import defaultfilters
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
-from django.core.validators import MaxLengthValidator, validate_slug, MaxValueValidator, MinValueValidator, \
-    FileExtensionValidator, ValidationError
-from django.template.defaultfilters import default, filesizeformat
+from django.core.validators import MaxValueValidator, MinValueValidator, FileExtensionValidator, ValidationError
 from django.contrib.auth.hashers import make_password
-from django.apps import apps
-
-from random import choice
-from string import ascii_lowercase, digits
 
 
 class UserManager(BaseUserManager):
@@ -97,31 +82,6 @@ def patogh_image_directory_path(instance, filename):
     return 'patogh/{0}/patogh_image/{1}'.format(str(instance.id), filename)
 
 
-class PatoghCategory(models.Model):
-    id = models.UUIDField(verbose_name=_("شناسه"), primary_key=True, default=uuid.uuid4,
-                          help_text="Unique Id for this Category")
-    name = models.CharField(max_length=50, verbose_name=_("کتگوری"))
-
-    class Meta:
-        ordering = ['id']
-        verbose_name = _('کتگوری')
-        verbose_name_plural = _('کتگوری ها')
-
-
-class LocationTypes(models.Model):
-    id = models.UUIDField(verbose_name=_("شناسه"), primary_key=True, default=uuid.uuid4,
-                          help_text="Unique Id for this location type")
-    name = models.CharField(max_length=30, verbose_name=_("نوع مکان"))
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ['id']
-        verbose_name = _('مکان')
-        verbose_name_plural = _('مکان ها')
-
-
 class Tags(models.Model):
     id = models.UUIDField(verbose_name=_("شناسه"), primary_key=True, default=uuid.uuid4,
                           help_text="Unique Id for this Tag")
@@ -133,7 +93,7 @@ class Tags(models.Model):
     class Meta:
         ordering = ['id']
         unique_together = ('id', 'tag')
-        verbose_name = _('برچست')
+        verbose_name = _('برچسب')
         verbose_name_plural = _('برچسب ها')
 
 
@@ -151,22 +111,8 @@ class City(models.Model):
         return self.name
 
 
-def generate_random_username(length=16, chars=ascii_lowercase + digits, split=4, delimiter='-'):
-    username = ''.join([choice(chars) for i in range(length)])
-
-    if split:
-        username = delimiter.join([username[start:start + split] for start in range(0, len(username), split)])
-
-    try:
-        User.objects.get(username=username)
-        return generate_random_username(length=length, chars=chars, split=split, delimiter=delimiter)
-    except User.DoesNotExist:
-        return username;
-
-
 class User(AbstractUser):
-    username = models.CharField(verbose_name=_("نام کاربری"), max_length=100, unique=True,
-                                default=generate_random_username)
+    username = models.CharField(verbose_name=_("نام کاربری"), max_length=100, unique=True)
     first_name = models.CharField(verbose_name=_("نام"), max_length=100, null=True, blank=True)
     last_name = models.CharField(verbose_name=_("نام خانوادگی"), max_length=100, null=True, blank=True)
     email = models.EmailField(verbose_name=_("ایمیل"), max_length=50, primary_key=True)
@@ -186,9 +132,9 @@ class User(AbstractUser):
     bio = models.CharField(verbose_name=_("درباره"), max_length=1000, null=True, blank=True)
     score = models.IntegerField(verbose_name=_("امتیاز کاربر"), null=True, blank=True, default=0)
     parties = models.ManyToManyField("Party", through="PartyMembers", blank=True)
+    friends = models.ManyToManyField('User', verbose_name='دوستان', blank=True)
 
     objects = UserManager()
-    friends = models.ManyToManyField('User', verbose_name='دوستان', blank=True)
 
     def __str__(self):
         return self.username
@@ -216,6 +162,71 @@ class PendingVerify(models.Model):
         verbose_name_plural = _('تاییدیه ها')
 
 
+class PatoghCategory(models.Model):
+    id = models.UUIDField(verbose_name=_("شناسه"), primary_key=True, default=uuid.uuid4,
+                          help_text="Unique Id for this Category")
+    name = models.CharField(max_length=50, verbose_name=_("کتگوری"))
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = _('کتگوری')
+        verbose_name_plural = _('کتگوری ها')
+
+
+class Patogh(models.Model):
+    id = models.UUIDField(primary_key=True, verbose_name=_("شناسه"), default=uuid.uuid4,
+                          help_text="Unique Id for this gathering")
+    creator = models.ForeignKey(User, verbose_name=_("شناسه پدید آورنده"), on_delete=models.PROTECT, null=True,
+                                blank=True)
+    name = models.CharField(verbose_name=_("نام"), max_length=50, null=True, blank=True)
+    profile_image = models.ImageField(verbose_name=_("عکس پاتوق"),
+                                      upload_to=patogh_image_directory_path,
+                                      null=True, blank=True, help_text=_("JPG, JPEG or PNG is validate"),
+                                      validators=[FileExtensionValidator(VALID_IMAGE_FORMAT), validate_image_size])
+    city = models.ForeignKey(City, verbose_name=_("شهر"), on_delete=models.PROTECT, null=True, blank=True)
+    description = models.CharField(verbose_name=_("توضیحات"), help_text="descripe your patogh", max_length=1000)
+    state = (
+        ('0', 'public'),
+        ('1', 'private')
+    )
+    type = models.CharField(verbose_name=_("حالت دورهمی"), default='0', choices=state, max_length=10)
+    creation_time = models.DateTimeField(verbose_name=_("زمان ساخت پاتوق"), help_text="Creation time for the patogh",
+                                         auto_now_add=True, null=True)
+    start_time = models.DateTimeField(verbose_name=_("زمان شروع"), help_text="Start time for the patogh", null=True)
+    end_time = models.DateTimeField(verbose_name=_("زمان پایان"), help_text="end time for the patogh", null=True)
+    category = models.ForeignKey(PatoghCategory, verbose_name=_("نوع پاتوق"), on_delete=models.PROTECT, null=True,
+                                 blank=True)
+    address = models.CharField(verbose_name=_("آدرس"), max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = _('پاتوق')
+        verbose_name_plural = _('پاتوق ها')
+
+
+class PatoghMembers(models.Model):
+    patogh_id = models.ForeignKey(Patogh, verbose_name=_("شناسه پاتوق"), on_delete=models.PROTECT, null=True)
+    email = models.ForeignKey(User, verbose_name=_("شناسه کاربر"), on_delete=models.PROTECT, null=True)
+    status = (
+        ('0', 'admin'),
+        ('1', 'normal participant')
+    )
+    state = models.CharField(verbose_name=_("مجوز کاربر"), choices=status, help_text="سطح دسترسی کاربر را مشخص کنید",
+                             default='1', max_length=10)
+    time = models.DateTimeField(verbose_name=_("زمان پیوستن"), help_text="attend patogh time", auto_now_add=True)
+
+    def __str__(self):
+        return self.patogh_id + " " + self.email
+
+    class Meta:
+        ordering = ['patogh_id']
+        verbose_name = _('عضو پاتوق')
+        verbose_name_plural = _('اعضای پاتوق')
+
+
 class Party(models.Model):
     id = models.UUIDField(primary_key=True, verbose_name=_("شناسه"), default=uuid.uuid4,
                           help_text="Unique Id for this party")
@@ -238,88 +249,9 @@ class Party(models.Model):
         verbose_name_plural = _('اکیپ ها')
 
 
-class PatoghInfo(models.Model):
-    id = models.UUIDField(primary_key=True, verbose_name=_("شناسه"), default=uuid.uuid4,
-                          help_text="Unique Id for this gathering")
-    creator = models.ForeignKey(User, verbose_name=_("شناسه پدید آورنده"), on_delete=models.PROTECT, null=True,
-                                blank=True)
-    name = models.CharField(verbose_name=_("نام"), max_length=50, null=True, blank=True)
-    profile_image = models.ImageField(verbose_name=_("عکس پاتوق"),
-                                      upload_to=patogh_image_directory_path,
-                                      null=True, blank=True, help_text=_("JPG, JPEG or PNG is validate"),
-                                      validators=[FileExtensionValidator(VALID_IMAGE_FORMAT), validate_image_size])
-    city = models.ForeignKey(City, verbose_name=_("شهر"), on_delete=models.PROTECT, null=True, blank=True)
-    description = models.CharField(verbose_name=_("توضیحات"), help_text="descripe your patogh", max_length=1000)
-    state = (
-        ('0', 'public'),
-        ('1', 'private')
-    )
-    type = models.CharField(verbose_name=_("حالت دورهمی"), default='0', choices=state, max_length=10)
-    creation_time = models.DateTimeField(verbose_name=_("زمان ساخت پاتوق"), help_text="Creation time for the patogh",
-                                         auto_now_add=True, null=True)
-    category = models.ForeignKey(PatoghCategory, verbose_name=_("نوع پاتوق"), on_delete=models.PROTECT, null=True,
-                                 blank=True)
-    address = models.CharField(verbose_name=_("آدرس"), max_length=1000, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ['id']
-        verbose_name = _('اطلاعات دورهمی')
-        verbose_name_plural = _('اطلاعات دورهمی ها')
-
-
-class Patogh(models.Model):
-    id = models.UUIDField(primary_key=True, verbose_name=_("شناسه"), default=uuid.uuid4,
-                          help_text="Unique Id for this gathering")
-    patogh_id = models.ForeignKey(PatoghInfo, verbose_name=_("شناسه اطلاعات پاتوق"), on_delete=models.PROTECT,
-                                  null=True)
-    start_time = models.DateTimeField(verbose_name=_("زمان شروع"), help_text="Start time for the patogh", null=True)
-    end_time = models.DateTimeField(verbose_name=_("زمان پایان"), help_text="end time for the patogh", null=True)
-
-    def __str__(self):
-        return self.id
-
-    @transaction.atomic
-    def save(self, *args, **kwargs):
-        if not hasattr(self, "patogh_id"):
-            self.patogh_id, _ = PatoghInfo.objects.get_or_create(id=self.id)
-
-        super(Patogh, self).save(*args, **kwargs)
-
-    # def create_patogh_patoghinfo(instance, created, raw, **kwargs):
-    #     # Ignore fixtures and saves for existing courses.
-    #     if not created or raw:
-    #         return
-
-    #     if not instance.patogh_id_id:
-    #         group, _ = PatoghInfo.objects.get_or_create(name='_course_' + self.id + '_student')
-    #         instance.student_group = group
-
-    #     if not instance.teacher_group_id:
-    #         teacher_group, _ = Group.objects.get_or_create(name='_course_' + self.id + '_teacher')
-    #         instance.teacher_group = teacher_group
-
-    #     instance.save()
-
-    # models.signals.post_save.connect(create_course_groups, sender=Course, dispatch_uid='create_course_groups')
-    # @transaction.atomic
-    # def save(self, *args, **kwargs):
-    #     if not self.patogh_id_id:
-    #         self.patogh_id, _ = PatoghInfo.objects.get_or_create(id= self.id)
-
-    #     super(Patogh, self).save(*args, **kwargs)
-
-    class Meta:
-        ordering = ['id']
-        verbose_name = _('دورهمی')
-        verbose_name_plural = _('دورهمی ها')
-
-
 class PartyMembers(models.Model):
-    p_id = models.ForeignKey(Party, verbose_name=_("اکیپ"), on_delete=models.PROTECT)
-    g_id = models.ForeignKey(User, verbose_name=_("کاربر"), on_delete=models.PROTECT)
+    party_id = models.ForeignKey(Party, verbose_name=_("اکیپ"), on_delete=models.PROTECT)
+    member_id = models.ForeignKey(User, verbose_name=_("کاربر"), on_delete=models.PROTECT)
     is_admin = (
         ('0', 'no'),
         ('1', 'yes')
@@ -327,91 +259,9 @@ class PartyMembers(models.Model):
     status = models.SmallIntegerField(verbose_name=_("سطح دسترسی کاربر به اکیپ"), choices=is_admin, default='0')
 
     class Meta:
-        unique_together = (("p_id", "g_id"))
-        ordering = ['p_id']
+        ordering = ['party_id']
         verbose_name = _('عضو اکیپ')
         verbose_name_plural = _('اعضای اکیپ')
-
-    def __str__(self):
-        return self.username.username
-
-
-class PatoghMembers(models.Model):
-    patogh_id = models.ForeignKey(PatoghInfo, verbose_name=_("شناسه پاتوق"), on_delete=models.PROTECT, null=True)
-    email = models.ForeignKey(User, verbose_name=_("شناسه کاربر"), on_delete=models.PROTECT, null=True)
-    status = (
-        ('0', 'admin'),
-        ('1', 'normal participant')
-    )
-    state = models.CharField(verbose_name=_("مجوز کاربر"), choices=status, help_text="سطح دسترسی کاربر را مشخص کنید",
-                             default='1', max_length=10)
-    time = models.DateTimeField(verbose_name=_("زمان پیوستن"), help_text="attend patogh time", auto_now_add=True)
-
-    def __str__(self):
-        return self.patogh_id + " " + self.email
-
-    class Meta:
-        ordering = ['patogh_id']
-        verbose_name = _('عضو دورهمی')
-        verbose_name_plural = _('اعضای دورهمی')
-
-
-class PatoghsComments(models.Model):
-    id = models.UUIDField(verbose_name=_("شناسه"), primary_key=True, default=uuid.uuid4,
-                          help_text="Unique Id for this patogh Commments")
-    sender = models.ForeignKey(User, verbose_name=_("فرستنده"), on_delete=models.PROTECT, null=True)
-    patogh_id = models.ForeignKey(Patogh, verbose_name=_("شناسه پاتوق"), on_delete=models.PROTECT, null=True)
-    reply_to = models.ForeignKey('self', verbose_name=_("بازخورد به"), on_delete=models.CASCADE)
-    send_time = models.DateTimeField(verbose_name=_("زمان ارسال"), auto_now_add=True, blank=True)
-    comment = models.CharField(verbose_name=_("نظر"), max_length=1000)
-
-    def __str__(self):
-        return self.id + " has commented " + self.comment
-
-    class Meta:
-        ordering = ['id']
-        verbose_name = _('نظر در مورد پاتوق')
-        verbose_name_plural = _('نظرات در مورد پاتوق')
-
-
-class reportedPatogh(models.Model):
-    patogh_id = models.ForeignKey(Patogh, verbose_name=_("شناسه پاتوق"), on_delete=models.PROTECT)
-    username = models.ForeignKey(User, verbose_name=_("نام کاربری"), on_delete=models.PROTECT)
-    massage = models.CharField(verbose_name=_("پیام"), max_length=1000)
-    send_time = models.DateTimeField(verbose_name=_("زمان ارسال"), auto_now_add=True, blank=True)
-
-    def __str__(self):
-        return self.patogh_id
-
-    class Meta:
-        ordering = ['patogh_id']
-        unique_together = ('patogh_id', 'username')
-        verbose_name = _('پاتوق گزارش شده')
-        verbose_name_plural = _('پاتوق های گزارش شده')
-
-
-class PatoghHaveImages(models.Model):
-    patogh_id = models.ForeignKey(Patogh, verbose_name=_("شناسه پاتوق"), on_delete=models.CASCADE)
-    image_url = models.ImageField(verbose_name=_("عکس برای دورهمی"),
-                                  upload_to=dorhami_image_profile_directory_path,
-                                  null=True, blank=True, help_text=_("JPG, JPEG or PNG is validate"),
-                                  validators=[FileExtensionValidator(VALID_IMAGE_FORMAT), validate_image_size])
-    state = (
-        (0, 'registered'),
-        (1, 'accepted'),
-        (2, 'rejected'),
-        (3, 'deleted')
-    )
-    status = models.SmallIntegerField(verbose_name=_("وضعیت تایید عکس"), choices=state, default=0)
-    send_time = models.DateTimeField(verbose_name=_("زمان ارسال"), auto_now_add=True, null=True, blank=True)
-
-    def __str__(self):
-        return self.patogh_id + " has " + self.state
-
-    class Meta:
-        ordering = ['patogh_id']
-        verbose_name = _('عکس پاتوق')
-        verbose_name_plural = _('عکس های پاتوق')
 
 
 class UsersHaveFriends(models.Model):
